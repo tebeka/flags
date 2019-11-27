3389bdf2d88e003eacea7660e19c5b2addc9404c
Miki Tebeka <miki.tebeka@gmail.com>
2019-11-27 08:48:02 +0200

    move example to example test

--- _examples/go.mod
+++
@@ -1,7 +0,0 @@
-module example
-
-go 1.13
-
-require github.com/tebeka/flags v0.1.0
-
-replace github.com/tebeka/flags => ../
--- example_test.go
+++ example_test.go
@@ -1,4 +1,4 @@
-package main
+package flags_test
 
 import (
 	"flag"
@@ -18,19 +18,36 @@ var config struct {
 	in    *os.File
 }
 
-func main() {
-	flag.Var(flags.Int(&config.port, checkPort), "port", "port to listen on")
-	flag.Var(flags.String(&config.name, checkName), "name", "logger name")
-	flag.Var(flags.URL(config.url), "url", "url to hit")
-	flag.Var(flags.Time(&config.start, time.RFC3339), "start", "start time")
-	flag.Var(flags.File(config.in, 'r'), "input", "input file")
-	flag.Parse()
+func ExampleUsage() {
+	fs := flag.NewFlagSet("example", flag.ContinueOnError)
+	fs.Var(flags.Int(&config.port, checkPort), "port", "port to listen on")
+	fs.Var(flags.String(&config.name, checkName), "name", "logger name")
+	fs.Var(flags.URL(config.url), "url", "url to hit")
+	fs.Var(flags.Time(&config.start, time.RFC3339), "start", "start time")
+	fs.Var(flags.File(config.in, 'r'), "input", "input file")
+
+	args := []string{
+		"-port", "999",
+		"-name", "lassie",
+		"-url", "http://example.com",
+		"-start", "2019-11-26T19:23:42Z",
+		"-input", "/dev/null",
+	}
+
+	fs.Parse(args)
 
 	fmt.Printf("port: %d\n", config.port)
 	fmt.Printf("name: %q\n", config.name)
 	fmt.Printf("url: %q\n", config.url.String())
 	fmt.Printf("start: %s\n", config.start)
 	fmt.Printf("in: %q\n", config.in.Name())
+
+	// Output:
+	// port: 999
+	// name: "lassie"
+	// url: "http://example.com"
+	// start: 2019-11-26 19:23:42 +0000 UTC
+	// in: "/dev/null"
 }
 
 func checkPort(p int) error {
