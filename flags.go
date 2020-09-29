5887dbd91d4bee3089d10648b14d87d3a43f33d1
Miki Tebeka <miki.tebeka@gmail.com>
2020-09-29 20:37:32 +0300

    nosec

--- flags.go
+++ flags.go
@@ -172,9 +172,9 @@ func (f *FileFlag) Set(s string) error {
 	case 'r':
 		file, err = os.Open(s) // #nosec
 	case 'w':
-		file, err = os.Create(s)
+		file, err = os.Create(s) // #nosec
 	case 'a':
-		file, err = os.OpenFile(s, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+		file, err = os.OpenFile(s, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600) // #nosec
 	}
 
 	if err != nil {
